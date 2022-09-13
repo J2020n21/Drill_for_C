6bac75fd1c32cab480c6d03a651d05ec013a4b7d
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2022-09-13 21:49:20 +0900

    Add files via upload
    I show the prime number through 3 to 100

---
+++ IsPrime.cpp
@@ -0,0 +1,25 @@
+﻿//finding prime number
+#include <iostream> 
+using namespace std;
+const int MAX = 100;
+
+int main() {
+    int start_num = 3;
+    int divider = 2;
+
+    while (start_num <= MAX) {
+        bool isprime = true;
+
+        for (; divider < start_num; divider++) {
+
+            if (start_num % divider == 0)
+                isprime = false;
+        }
+
+        if (isprime)
+            cout << start_num << endl;
+
+        start_num++;
+        divider = 2;
+    }
+}
