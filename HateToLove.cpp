b25a56407f70d330bd977d060a8029fca915dfe0
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2022-09-13 22:14:57 +0900

    Add files via upload
    In example.txt, there are three "hate". 
    The purpose of this program is to change the "hate" to "love" when it prints out to the console.
    Need more practice!

---
+++ HateToLove.cpp
@@ -0,0 +1,29 @@
+﻿//file input and output: change "hate" to "love" only in console output
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	string str;
+	string replaced_txt;
+
+	fstream readstream;
+	readstream.open("example.txt");
+
+	while (readstream >> str) {
+		//cout << str; //output check
+
+		if (str == "hate") { //print "love" rather "hate"
+			cout << "love ";
+		}
+
+		else {
+			cout << str << " ";
+
+		}
+	}
+
+	readstream.close();
+
+}
